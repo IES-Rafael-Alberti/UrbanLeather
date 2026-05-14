0ba418ae2edbe21b53e1677344eaee5a60480fba
rramigl316 <rramigl316@g.educaand.es>
2026-05-14 20:15:55 +0200

    imagenes update, iconos

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8081",
+});
+
+// =========================
+// INTERCEPTOR REQUEST JWT
+// =========================
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (
+    token &&
+    token !== "null" &&
+    token !== "undefined"
+  ) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// =========================
+// INTERCEPTOR RESPONSE
+// =========================
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+
+    if (error.response?.status === 401) {
+
+      // token inválido o expirado
+      localStorage.removeItem("token");
+
+      // redirigir login
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
